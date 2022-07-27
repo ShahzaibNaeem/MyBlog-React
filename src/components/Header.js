b435081bea9a023b69699cf81643b754a890bc88
Shahzaib <chorli635@gmail.com>
2022-07-27 12:44:17 +0500

    Navbar Created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,32 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'//Navlink by default gives "active" class   
 import Navbar from './Navbar'
+import styled from 'styled-components'
 const Header = () => {
   return (
     <>
-      <header>
-        <NavLink  to="/"><h3>Shahzaib Naeem</h3></NavLink>
+      <Mainheader>
+        <NavLink  to="/"><span>SHAHZAIB BLOG</span></NavLink>
         <Navbar/>
-      </header>  
+      </Mainheader>  
     </>
   )
 }
+
+const Mainheader=styled.header`
+display:flex;
+justify-content:space-between;
+align-items:center;
+background-color:${({theme})=>theme.colors.bg};
+padding:0 4.8rem;
+height:10rem;
+
+
+span{
+  
+  font-size:3rem;
+  font-weight:900;
+}
+`
 
 export default Header
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,41 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
 
 const Navbar = () => {
   return (
     <>
-    <nav>
+    <Nav>
        <div className="menuIcon">
         <ul className="navbar-list">
-         <li><NavLink to="/">Home</NavLink></li>
-         <li><NavLink to="/about">About</NavLink></li>
-         <li><NavLink to="/services">Services</NavLink></li>
-         <li><NavLink to="/contact">Contact</NavLink></li>
+         <li><NavLink to="/" className="navbar-link">Home</NavLink></li>
+         <li><NavLink to="/about" className="navbar-link">About</NavLink></li>
+         <li><NavLink to="/services" className="navbar-link">Services</NavLink></li>
+         <li><NavLink to="/contact" className="navbar-link">Contact</NavLink></li>
         </ul>
         </div>
-     </nav>
+     </Nav>
     </>
   )
 }
+
+const Nav=styled.nav`
+.navbar-list{
+  display:flex;
+  gap:4.8rem;
+}
+ .navbar-link{
+  &:link,&:visited{                       /* & represents the parent element*/
+    color:${({theme})=>theme.colors.black};
+    font-size:1.8rem;
+    text-transform:uppercase;
+    transition:color 0.3s linear           /*specific property transition */
+  }
+  &:hover,&:active{
+    color:${({theme})=>theme.colors.helper}
+  }
+ }
+
+`
 
 export default Navbar
